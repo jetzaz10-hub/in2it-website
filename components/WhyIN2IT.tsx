f600f96e79c60c39e11fb504c96f7c60c0385c3e
IN2IT Developer <dev@in2it.co.th>
2026-04-29 15:52:28 +0700

    Insert black gradient transition masks to WhyIN2IT to blend with adjacent sections

--- components/WhyIN2IT.tsx
+++ components/WhyIN2IT.tsx
@@ -85,6 +85,12 @@ export default function WhyIN2IT() {
         <div className="absolute top-1/4 left-[-10%] w-[600px] h-[600px] bg-[#9933FF]/35 rounded-full blur-[100px] animate-pulse" style={{ animationDuration: '8s' }} />
         <div className="absolute bottom-1/4 right-[-10%] w-[700px] h-[700px] bg-[#4A32FF]/35 rounded-full blur-[120px] animate-pulse" style={{ animationDuration: '10s' }} />
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[800px] h-[800px] bg-[#FF3366]/15 rounded-full blur-[100px]" />
+        
+        {/* Top black fade-in mask */}
+        <div className="absolute top-0 left-0 right-0 h-40 bg-gradient-to-b from-black to-transparent z-10 pointer-events-none" />
+        
+        {/* Bottom black fade-out mask */}
+        <div className="absolute bottom-0 left-0 right-0 h-40 bg-gradient-to-t from-black to-transparent z-10 pointer-events-none" />
       </div>
 
       <div className="max-w-[1440px] mx-auto px-6 relative z-10">
